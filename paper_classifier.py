2bd3ff822bbf407e22d99220fb028f365a6118ac
Tom Davidson <trd54@cornell.edu>
2016-09-26 21:21:51 -0400

    updated classifier code

--- paper_classifier.py
+++ paper_classifier.py
@@ -8,6 +8,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import linear_model
 from sklearn.feature_selection import SelectFromModel
 from sklearn.cross_validation import KFold, StratifiedKFold
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 from sklearn import metrics
 from paper_stats import *
 
@@ -38,7 +40,7 @@ vectorizer = TfidfVectorizer(
     decode_error='replace'
     )
 
-def kfold(X, y, model, K):
+def kfold(X, y, model, K, multi_flag):
     kf=StratifiedKFold(y, n_folds=K, random_state=42, shuffle=False)
     k=1
     precision = 0
@@ -52,23 +54,42 @@ def kfold(X, y, model, K):
         print "Fitting model ", k
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        p = metrics.precision_score(y_test, y_pred)
-        r = metrics.recall_score(y_test, y_pred)
-        f1 = metrics.f1_score(y_test, y_pred)
+        if multi_flag is True:
+            p = metrics.precision_score(y_test, y_pred, average='weighted')
+            r = metrics.recall_score(y_test, y_pred, average='weighted')
+            f1 = metrics.f1_score(y_test, y_pred, average='weighted')
+        else:
+            p = metrics.precision_score(y_test, y_pred)
+            r = metrics.recall_score(y_test, y_pred)
+            f1 = metrics.f1_score(y_test, y_pred)
         print p, r, f1
         precision += p
         recall += r
         accuracy += f1
         k+=1
+    print "Precision: ", precision/K, " Recall: ", recall/K, " F1: ", accuracy/K
     return precision/K, recall/K, accuracy/K
+
+def get_sections(section_ids):
+    """Takes a list of section ids and returns a
+    dataframe containing the students & essays
+    that need to be graded"""
+    sections = []
+    for i in section_ids:
+        df_section = df[df.section == i]
+        sections.append(df_section)
+    sections_df = pd.concat(sections)
+    to_grade_df = sections_df[sections_df.grade == 66]
+    print to_grade_df.shape[0], " essays to grade."
+    return to_grade_df
 
 def model_iterator(df, model, K):
     X_strings = df['essay']
-    print "Running models to predict excellent using stats only"
+    #print "Running models to predict excellent using stats only"
     y = df['excellent']
     X2 = get_stats(list(df['essay']))
-    a = kfold(X2, y, model, K)
-    print a
+    #a = kfold(X2, y, model, K)
+    #print a
     #print "Running models to predict satisfactory..."
     #y = df['satisfactory']
     #X = vectorizer.fit_transform(X_strings, y) # fit vectorizer here
@@ -81,17 +102,12 @@ def model_iterator(df, model, K):
     #X = pd.DataFrame(X.toarray())
     #c = kfold(X, y, model, K)
     #print c
-    print "Running models to predict excellent using vectorizer"
-    y = df['excellent']
+    #print "Running models to predict excellent using vectorizer"
+    #y = df['excellent']
     X = vectorizer.fit_transform(X_strings, y) # fit vectorizer here
     X = pd.DataFrame(X.toarray())
-    d = kfold(X, y, model, K)
-    print d
-
-    print type(X)
-    print X.shape
-    print type(X2)
-    print X2.shape
+    #d = kfold(X, y, model, K)
+    #print d
 
     print "Running models to predict excellent using both"
     M = pd.concat([X,X2], axis=1)
@@ -101,10 +117,12 @@ def model_iterator(df, model, K):
     #Skipping kfold above as slow!
     model = linear_model.LogisticRegression(C=10.0, penalty='l1',
                                             class_weight='balanced')
-    model.fit(M, y)
+
+    P = OneVsRestClassifier(model)
+    P.fit(M, y)
 
     print "Now performing feature selection to get better results"
-    best = SelectFromModel(model, prefit=True,
+    best = SelectFromModel(P, prefit=True,
                         threshold="mean")
 
     X_new = best.transform(M)
@@ -112,7 +130,8 @@ def model_iterator(df, model, K):
     LR_2 = linear_model.LogisticRegression(C=10.0, penalty='l2',
                                          #regularization l1 since data are sparse
                                         class_weight='balanced')
-    final = kfold(X_new, y, LR_2, 10)
+    P = OneVsRestClassifier(LR_2)
+    final = kfold(X_new, y, P, 10, True)
     print final
 
 def prediction(df, to_grade, category_to_predict, week):
@@ -159,19 +178,28 @@ def prediction(df, to_grade, category_to_predict, week):
     name = 'graded_'+category_to_predict+"_"+week
     pickle.dump(graded, open('graded_wk4.p', 'wb'))
 
+def prediction_onevsrest(df, to_grade, category_to_predict, week):
+    data = pd.concat([df, to_grade])
+    X_strings = data['essay']
+    X = vectorizer.fit_transform(X_strings) # fit vectorizer here
+    X = pd.DataFrame(X.toarray())
+    X2 = get_stats(list(data['essay']))
+    M = pd.concat([X,X2], axis=1)
+    y = data[category_to_predict]
+    num2predict = - to_grade.shape[0]
+    X_test, X_train = M.iloc[:-num2predict], M.iloc[-num2predict:]
+    print X_train.shape, X_test.shape
+    y_test, y_train = y.iloc[:-num2predict], y.iloc[-num2predict:]
+    print y_train.shape, y_test.shape
+    print "Running first model on training set"
+    LR = linear_model.LogisticRegression(C=10.0, penalty='l1',
+    class_weight='balanced')
+    P = OneVsRestClassifier(LR)#.fit(X_train, y_train)
+    print "Using trained model to predict for test set.."
+    kfold(X_train, y_train, P, 10, True)
+    y_pred = P.predict(X_test)
+    print y_pred
 
-def get_sections(section_ids):
-    """Takes a list of section ids and returns a
-    dataframe containing the students & essays
-    that need to be graded"""
-    sections = []
-    for i in section_ids:
-        df_section = df[df.section == i]
-        sections.append(df_section)
-    sections_df = pd.concat(sections)
-    to_grade_df = sections_df[sections_df.grade == 66]
-    print to_grade_df.shape[0], " essays to grade."
-    return to_grade_df
 
 if __name__ == '__main__':
     df = pickle.load(open('week4_model_table.p', 'rb'))
@@ -186,8 +214,8 @@ if __name__ == '__main__':
     df = df[df.grade != 0] #Remove zero entries
     df = df[df.grade != 66] ##Remove ungraded (for now)
     print df.shape
-    #df.grade.hist(bins=20)
-    #plt.show()
+    df.grade.hist(bins=20)
+    plt.show()
 
     # Split into features and target
     K = 10 #10 folds
@@ -195,8 +223,8 @@ if __name__ == '__main__':
     model = linear_model.LogisticRegression(C=10.0, penalty='l1',
                                             class_weight='balanced')
 
-    #####model_iterator(df, model, K)
+    model_iterator(df, model, K)
     #lr_kf = kfold(X, y, model, 10)
     #print lr_kf
-    PRED_CATEGORY = 'good'
-    prediction(df, to_grade, PRED_CATEGORY , WEEK)
+    ###PRED_CATEGORY = 'good'
+    ####prediction(df, to_grade, PRED_CATEGORY , WEEK)
